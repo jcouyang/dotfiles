5be5f8106a4cb2ecfdc095b176467048314d5e84
Jichao Ouyang <oyanglulu@gmail.com>
2016-01-06 22:02:12 +0800

    delete default offset

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -3,23 +3,17 @@
 (require 'org)
 (setq org-confirm-babel-evaluate nil
       vc-follow-symlinks nil)
+
+(org-babel-load-file "~/.emacs.d/config.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
- '(electric-pair-mode t)
- '(js2-basic-offset 2 t)
- '(standard-indent 2)
- '(web-mode-code-indent-offset 2)
- '(web-mode-markup-indent-offset 4))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(org-babel-load-file "~/.emacs.d/config.org")
