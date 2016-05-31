5e9694527a929507b30cd445af407d0df7163a21
Jichao Ouyang <oyanglulu@gmail.com>
2016-05-31 10:04:35 +0800

    update hammerspoon

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -15,7 +15,7 @@ local applescript = require "hs.applescript"
 grid.setMargins({0,0})
 grid.setGrid'4x3'
 -- constants
-local smash = {"cmd", "ctrl" }
+local smash = {"cmd", "alt" }
 local hulk_smash = {"ctrl", "cmd", "alt"}
 local monitor = "LCD"
 local second_monitor = "extended LCD"
@@ -27,7 +27,7 @@ hotkey.bind(smash, "j", grid.pushWindowDown)
 hotkey.bind(smash, "k", grid.pushWindowUp)
 hotkey.bind(smash, "t", grid.resizeWindowThinner)
 hotkey.bind(smash, "w", grid.resizeWindowWider)
-hotkey.bind(smash, "i", grid.resizeWindowTaller)
+hotkey.bind(hulk_smash, "i", grid.resizeWindowTaller)
 hotkey.bind(smash, "m", grid.resizeWindowShorter)
 hotkey.bind(hulk_smash, "f", grid.maximizeWindow)
 hotkey.bind({"cmd"}, "e", hs.hints.windowHints)
