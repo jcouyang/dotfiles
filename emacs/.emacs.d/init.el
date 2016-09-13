63100c90d0543e3f2e4c9eabb5793854dbe824ad
Jichao Ouyang <oyanglulu@gmail.com>
2016-09-13 22:04:22 +0800

    config ruby and org deck

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -10,7 +10,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-agenda-files
+   (quote
+    ("~/Dropbox/org/agendas.org" "~/Dropbox/org/refile.org" "~/Dropbox/org/s-99.org" "~/Dropbox/org/todo.org")))
+ '(org-deck-base-url "http://imakewebthings.com/deck.js/")
+ '(org-trello-files (\` ((\, org-default-notes-file)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
