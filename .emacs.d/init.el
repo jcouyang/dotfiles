6e2457a6a1916b7f2bf35e9b26c7a8702c90e0c9
Jichao Ouyang <oyanglulu@gmail.com>
2019-04-13 12:04:48 +1000

    switch to use-package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,7 +4,7 @@
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
-(require 'cask "/usr/local/opt/cask/cask.el")
+(require 'cask "~/.cask/cask.el")
 (cask-initialize)
 (require 'org)
 (setq org-confirm-babel-evaluate nil
@@ -18,7 +18,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (rust-mode yaml-mode web-mode visual-regexp use-package unicode-fonts smartparens rainbow-mode pallet ox-tufte on-screen nyan-mode multiple-cursors markdown-mode magit langtool key-chord json-mode js2-mode indent-guide iedit helm-projectile helm-ag fasd expand-region ensime dracula-theme ag ace-jump-mode))))
+    (rust-mode yaml-mode web-mode visual-regexp use-package unicode-fonts smartparens rainbow-mode pallet ox-tufte on-screen nyan-mode multiple-cursors markdown-mode langtool key-chord json-mode js2-mode indent-guide iedit helm-projectile helm-ag fasd expand-region ensime dracula-theme ag ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
