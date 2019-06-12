f214a5ea8871885454afb288d05cff7cbc457836
Jichao Ouyang <oyanglulu@gmail.com>
2019-06-12 10:43:04 +1000

    update graalvm env

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,11 +6,11 @@ set -x LANG en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
 
 # Java
-set -x JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_151.jdk/Contents/Home
+set -x JAVA_HOME /Applications/Graalvm/Contents/Home
 # emacs
 set -x EDITOR emacsclient
 
-set -gx PATH /usr/local/texlive/2018/bin $PATH
+set -gx PATH $PATH /Applications/Graalvm/Contents/Home/bin /Applications/Graalvm/Contents/Home/jre/languages/js/bin/ /usr/local/texlive/2018/bin
 
 # rust
 set -x PATH $HOME/.cargo/bin $PATH
