4fe33a33b143ea1e1b2ed2450cac77e47167022c
Jichao Ouyang <oyanglulu@gmail.com>
2020-10-03 19:00:46 +1000

    update nixpkgs to 20.09

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,20 +1,11 @@
 {
-  nixpkgs.config.allowUnsupportedSystem = true;
-  packageOverrides = pkgs: let
-      easy_dhall = import (pkgs.fetchFromGitHub {
-    owner = "justinwoo";
-    repo = "easy-dhall-nix";
-    rev = "7c22a145fcb8e00b61d29efc7543af0c80d709ed";
-    sha256 = "03qry1wbhyb8i2qz7gcb04hw5d75a4vx2d4xjb6iradj1pc199xg";
-      }) {};
-      in with pkgs; {
+  packageOverrides = pkgs: with pkgs; {
     oyanglulu = pkgs.buildEnv {
-      name = "oyanglulu-pkgs";
+      name = "oyanglulu-env";
       meta = {
         priority = 10;
       };
       paths = [
-        fish
         silver-searcher
         fasd
         jq
@@ -30,15 +21,9 @@
         sbt
         # JS
         yarn
-        easy_dhall.dhall-simple
-        easy_dhall.dhall-lsp-simple
-        easy_dhall.dhall-json-simple
+        dhall
+        dhall-json
       ];
-      postBuild = ''
-      echo "linking fish"
-      test -e /usr/local/bin/fish || ln -s $out/bin/fish /usr/local/bin/fish
-      test -e /usr/local/bin/fasd || ln -s $out/bin/fasd /usr/local/bin/fasd
-      '';
     };
   };
 }
