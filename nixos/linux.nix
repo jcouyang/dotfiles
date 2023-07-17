b33294b6f3d9a8f10bd4c8cd856aaca3a7aa955a
Jichao Ouyang <oyanglulu@gmail.com>
2023-07-17 22:11:10 +1000

    upgrade 23.05

--- nixos/linux.nix
+++ nixos/linux.nix
@@ -49,7 +49,7 @@
       enable = true;
       dockerCompat = true;
       dockerSocket.enable = true;
-      defaultNetwork.dnsname.enable = true;
+      defaultNetwork.settings.dns_enabled = true;
     };
   };
 
